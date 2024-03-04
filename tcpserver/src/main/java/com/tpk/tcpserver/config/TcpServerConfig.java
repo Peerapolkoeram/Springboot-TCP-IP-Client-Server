8fa7f99aec746d0cd778137ec1e3b34c6205a900
peerapolkoeram <peerapolkoeram@gmail.com>
2024-03-05 00:07:17 +0700

    Update TcpServerConfig

--- tcpserver/src/main/java/com/tpk/tcpserver/config/TcpServerConfig.java
+++ tcpserver/src/main/java/com/tpk/tcpserver/config/TcpServerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.ip.tcp.TcpReceivingChannelAdapter;
 import org.springframework.integration.ip.tcp.connection.TcpNetServerConnectionFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.support.MessageBuilder;
 
 @Configuration
 public class TcpServerConfig {
@@ -32,8 +36,19 @@ public class TcpServerConfig {
     public MessageHandler handler() {
         return message -> {
             System.out.println("Received: " + message);
-            // Handle the received message here
+            Message<String> responseMessage = MessageBuilder.withPayload("Response message").build();
+            outputChannel().send(responseMessage);
         };
+    }
+
+    @Bean
+    public MessageChannel outputChannel() {
+        return new DirectChannel();
+    }
+
+    @ServiceActivator(inputChannel = "outputChannel")
+    public void handleMessage(Message<?> message) {
+        System.out.println(message.getPayload());
     }
 
 }
